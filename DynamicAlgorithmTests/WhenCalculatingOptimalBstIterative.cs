f0d10fe5522d6661ade3ca2fade151792f6e4c0d
robert.c.phillips@live.com <robert.c.phillips@live.com>
2014-07-20 13:45:47 -0500

    Adding bst solver for problem set 3.

---
+++ DynamicAlgorithmTests/WhenCalculatingOptimalBstIterative.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using DynamicAlgorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace DynamicAlgorithmTests
+{
+    public class WhenCalculatingOptimalBstIterative
+    {
+        [TestClass]
+        public class WhenUsingPs
+        {
+            [TestMethod]
+            public void The_result_should_be_2_point_18()
+            {
+                var input = new[]
+                {
+                    new[]{1, .05},
+                    new[]{2, .4},
+                    new[]{3, .08},
+                    new[]{4, .04},
+                    new[]{5, .1},
+                    new[]{6, .1},
+                    new[]{7, .23}
+                };
+
+                var solver = new OptimalBstIterativeSolver(input);
+                var result = Math.Round(solver.Solve(), 2);
+                result.ShouldEqual(2.18);
+            }
+        }
+
+        [TestClass]
+        public class WhenUsingVl01
+        {
+            [TestMethod]
+            public void The_result_should_be_1_point_3()
+            {
+                var input = new[]
+                {
+                    new[]{1, .8},
+                    new[]{2, .1},
+                    new[]{3, .1}
+                };
+
+                var solver = new OptimalBstIterativeSolver(input);
+                var result = solver.Solve();
+                result.ShouldEqual(1.3);
+            }
+        }
+
+        [TestClass]
+        public class WhenUsingOneItem
+        {
+            [TestMethod]
+            public void Should_return_the_item()
+            {
+                var input = new[] {new[] {1, 1.0}};
+
+                var solver = new OptimalBstIterativeSolver(input);
+                var result = solver.Solve();
+                result.ShouldEqual(1.0);
+            }
+        }
+
+        [TestClass]
+        public class WhenUsingTwoItems
+        {
+            [TestMethod]
+            public void Should_return_the_larger_item_plus_2_times_smaller_item()
+            {
+                var input = new[]
+                {
+                    new[] { 1, .8 },
+                    new[] { 2, .2 }
+                };
+
+                var solver = new OptimalBstIterativeSolver(input);
+                var result = solver.Solve();
+                result.ShouldEqual(1.2);
+            }
+        }
+    }
+}
---
+++ DynamicAlgorithms/OptimalBstIterativeSolver.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+namespace DynamicAlgorithms
+{
+    public class OptimalBstIterativeSolver
+    {
+        private readonly double[][] _keys;
+
+        public OptimalBstIterativeSolver(double[][] keys)
+        {
+            _keys = keys;
+        }
+
+        public double Solve()
+        {
+            var n = _keys.Length;
+
+            var table = new double[n + 1][];
+            for (var ki = 0; ki <= n; ki++)
+            {
+                table[ki] = new double[n + 1];
+            }
+
+            for (var s = 0; s <= (n - 1); s++)
+            {
+                //for (var i = 1; i <= n; i++)
+                for (var i = 1; i <= n-s; i++)
+                {
+                    var j = i + s;
+                    table[i][j] = Min(table, i, j);
+                }
+            }
+
+            return table[1][n];
+        }
+
+        private double Min(double[][] table, int i, int j)
+        {
+            var min = double.MaxValue;
+
+            for (var r = i; r <= j; r++)
+            {
+                var temp = WeightSum(i, j);
+                temp += i > (r - 1) ? 0 : table[i][r - 1];
+                temp += (r + 1) > j ? 0 : table[r + 1][j];
+
+                if (temp < min)
+                {
+                    min = temp;
+                }
+            }
+
+            return min;
+        }
+
+        private double WeightSum(int i, int j)
+        {
+            var sum = _keys.Skip(i-1).Take(j-i+1).Sum(k => k[1]);
+            return sum;
+        }
+    }
+}
